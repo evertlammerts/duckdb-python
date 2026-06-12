ecbebcbebd19e2fd3cb617d09e1c1e9104c0bf6b
Evert Lammerts <evert.lammerts@gmail.com>
2026-06-12 22:25:43 +0200

    add timestamp_tz_ns support to the pyspark module

--- duckdb/experimental/spark/sql/types.py
+++ duckdb/experimental/spark/sql/types.py
@@ -49,6 +49,7 @@ __all__ = [
     "TimestampMillisecondNTZType",
     "TimestampNTZType",
     "TimestampNanosecondNTZType",
+    "TimestampNanosecondType",
     "TimestampSecondNTZType",
     "TimestampType",
     "UUIDType",
@@ -237,6 +238,26 @@ class TimestampType(AtomicType, metaclass=DataTypeSingleton):
         if ts is not None:
             # using int to avoid precision loss in float
             return datetime.datetime.fromtimestamp(ts // 1000000).replace(microsecond=ts % 1000000)
+
+
+class TimestampNanosecondType(AtomicType, metaclass=DataTypeSingleton):
+    """Timestamp (datetime.datetime) data type with timezone information with nanosecond precision."""
+
+    def __init__(self) -> None:  # noqa: D107
+        super().__init__(DuckDBPyType("TIMESTAMPTZ_NS"))
+
+    def needConversion(self) -> bool:  # noqa: D102
+        return True
+
+    @classmethod
+    def typeName(cls) -> str:  # noqa: D102
+        return "timestamptz_ns"
+
+    def toInternal(self, dt: datetime.datetime) -> int:  # noqa: D102
+        raise ContributionsAcceptedError
+
+    def fromInternal(self, ts: int) -> datetime.datetime:  # noqa: D102
+        raise ContributionsAcceptedError
 
 
 class TimestampNTZType(AtomicType, metaclass=DataTypeSingleton):
