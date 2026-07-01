50d1f00dcd7c71b79a211f0aa28129c0f4620c45
Evert Lammerts <evert.lammerts@gmail.com>
2026-07-01 13:36:27 +0200

    fix format

--- src/duckdb_py/pyrelation.cpp
+++ src/duckdb_py/pyrelation.cpp
@@ -1550,8 +1550,9 @@ unique_ptr<DuckDBPyRelation> DuckDBPyRelation::Query(const string &view_name, co
 	auto &statement = *parser.statements[0];
 	if (statement.type == StatementType::SELECT_STATEMENT) {
 		auto select_statement = unique_ptr_cast<SQLStatement, SelectStatement>(std::move(parser.statements[0]));
-		auto query_relation = make_shared_ptr<QueryRelation>(rel->context->GetContext(), std::move(select_statement),
-		                                                     "query_relation_" + StringUtil::GenerateRandomName(16), sql_query);
+		auto query_relation =
+		    make_shared_ptr<QueryRelation>(rel->context->GetContext(), std::move(select_statement),
+		                                   "query_relation_" + StringUtil::GenerateRandomName(16), sql_query);
 		return DeriveRelation(std::move(query_relation));
 	} else if (IsDescribeStatement(statement)) {
 		auto query = PragmaShow(view_name);
